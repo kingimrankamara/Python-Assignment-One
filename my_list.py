1dcfc5266a9001dbfc096a44259ee74651c5addb
Kingimrankamara <hassanimran.kamara@gmail.com>
2024-12-24 07:33:07 +0000

    Assignment two

---
+++ my_list.py
@@ -0,0 +1,28 @@
+# my empty list
+my_list = []
+
+# Append elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert the value 15 at the second position
+my_list.insert(1, 15)
+
+# Extend my_list with another list [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# Remove the last element from my_list
+my_list.pop()
+
+# Sort my_list in ascending order
+my_list.sort()
+
+# Find and print the index of the value 30 in my_list
+index_of_30 = my_list.index(30)
+print(f"The index of the value 30 in my_list is: {index_of_30}")
+
+# Output the final list and the index of 30
+print(my_list)
+
